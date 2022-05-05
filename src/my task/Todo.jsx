d2d0d39bff6df072a8f0bb9adf934b3d34c5176d
sanjit <sanjit.kissan@gmail.com>
2022-05-05 14:42:14 +0530

    complited

--- src/my task/Todo.jsx
+++ src/my task/Todo.jsx
@@ -1,5 +1,7 @@
-import React,{useState, useRef} from 'react'
+import React, { useState, useRef } from 'react'
 import "./style.css"
+
+
 export default function Todo() {
     const [data, setData] = useState([])
     const [comment, setComment] = useState([])
@@ -9,110 +11,130 @@ export default function Todo() {
     const dateRef = useRef();
     const statusRef = useRef();
 
-    function submit(event){
+    function submit(event) {
         event.preventDefault()
         let details = {
-            comment:event.target.comment.value,
-            date:event.target.date.value,
-            status:event.target.status.value
-            
+            comment: event.target.comment.value,
+            date: event.target.date.value,
+            status: event.target.status.value
+
         }
-        let newComment = [...data,details]
+        let newComment = [...data, details]
         console.log(newComment)
         setData(newComment)
 
     }
-    function searchData(){
+    function searchData() {
         let date = document.getElementById("dateTwo").value;
         console.log(date)
-        let newData = data.filter((val,index)=>{
-            if (val.date == date){
+        let newData = data.filter((val, index) => {
+            if (val.date == date) {
                 return true;
-            }else{
+            } else {
                 return false
-                
+
             }
         })
         setComment(newData)
     }
 
-    function editComment(i){
+    function editComment(i) {
         console.log(i)
         setEditeIndex(i);
-        let filterData = data.filter((val,index)=>{
-            if (index==i){
+        let filterData = data.filter((val, index) => {
+            if (index == i) {
                 return true;
             }
         })
         console.log(filterData);
 
-        commentRef.current.value="";
+        commentRef.current.value = "";
         dateRef.current.value = "";
         statusRef.current.value = "";
     }
-    function updateData (event){
+    function updateData(event) {
         event.preventDefault()
-        let newInfo = data.map((val, index)=>{
-            if(index==editeIndex){
+        let newInfo = data.map((val, index) => {
+            if (index == editeIndex) {
                 console.log(val);
-                val.comment=commentRef.current.value;
-                val.date=dateRef.current.value;
+                val.comment = commentRef.current.value;
+                val.date = dateRef.current.value;
                 val.status = statusRef.current.value;
             }
             return val;
         })
         setData(newInfo);
-        commentRef.current.val="";
+        commentRef.current.val = "";
         dateRef.current.value = "";
         statusRef.current.value = "";
     }
 
-    function deleteComment(index){
+    function deleteComment(index) {
         let newCooment = [...data];
         delete newCooment[index];
-        let modifiedComment = newCooment.filter((item)=>item);
+        let modifiedComment = newCooment.filter((item) => item);
         setData(modifiedComment)
     }
-    
+    function deleteCommentOne(index) {
+        let newCooment = [...comment];
+        delete newCooment[index];
+        let modifiedComment = newCooment.filter((item) => item);
+        setComment(modifiedComment)
+    }
 
-  return (
-    <div>
-        <form id='formOne' onSubmit={submit}>
-       Enter your comment :  <input type="text" className="input-group mb-3" name="comment" id="comment" ref={commentRef}/>
-       Select the date :  <input type="date" className="input-group mb-3" name="date" id="date" ref={dateRef} />
-       Selecte the option : <select name="status" className="input-group mb-3" id="" ref={statusRef}>
-            <option value="select">Select option</option>
-            <option value="complited">Complited</option>
-            <option value="Incomplited">Incomplited</option>
-        </select>
-        
-        {editeIndex != null ? <button type='button' className="btn btn-primary" onClick={updateData}>Update</button> : <button className='btn btn-primary'>Add comment</button> }
-        
-        </form>
 
-         <br />
+    return (
+        <div>
+            <div className="nav">
+                <input type="date" name="search" id='dateTwo' className='search-tag' placeholder='enter date to search comment'/>
+                <span class="input-group-btn">
+        <button class="btn btn-primary" type="button" onClick={searchData}>search!</button>
+      </span>
+                
+            </div>
 
-        Enter the date to search the comment : <input type="date" name="search" className="input-group mb-3" id='dateTwo' />
-        <button type='button' className='btn btn-primary' onClick={searchData} >Search</button>
+            <div className='comment-div'>
+                {comment != undefined ? comment.map((val, index) => {
+                    return <ul>
+                        <li id='commentli' >{"comment:- "}{val.comment} <br />{"date:-"}{val.date} <br />{"status :-"}{val.status} <br /> <button className="btn btn-primary" onClick={() => editComment(index)}>Edit</button>
+                            {"  "} <br /><button type='button' className="btn btn-primary" onClick={() => deleteCommentOne(index)}>Delete</button></li>
+                    </ul>
+                }) : <h2> there is no comment on this date please add a comment </h2>}
+            </div>
+                
 
-      <div>
-          {comment!= undefined ? comment.map((val, index)=>{
-              return <ul>
-                  <li id='commentli' className="list-group-item">{val.comment} <br />{val.date} <br />{val.status} <br /> <button className="btn btn-primary" onClick={()=>editComment(index)}>Edit</button>
-                    {"  "} <br /><button type='button' className="btn btn-primary" onClick={()=>deleteComment(index)}>Delete</button></li>
-              </ul>
-          }): <h2> there is no comment on this date please add a comment </h2> }
-      </div>
-        {
-            data.map((val,index)=>{
-                return <ul>
-                    <li id='lisiOne' className="list-group-item">{val.comment} {"  "} <br /> {val.date}{"  "} <br /> {val.status}{"  "} <br/>
-                    <button className="btn btn-primary" onClick={()=>editComment(index)}>Edit</button>
-                    {"  "} <br /><button type='button' className="btn btn-primary" onClick={()=>deleteComment(index)}>Delete</button> </li>
-                </ul>
-            })
-        }
+            <div className="input-tag">
+                <form id='formOne' onSubmit={submit}>
+                    <input type="text" name="comment" id="comment" ref={commentRef} placeholder="Enter your comment" /><br />
+                    <input type="date" name="date" id="date" ref={dateRef} placeholder="Select the date" /><br />
+                    <select name="status" id="select" ref={statusRef} placeholder="Selecte the option">
+                        <option value="select">Select option</option>
+                        <option value="complited">Complited</option>
+                        <option value="Incomplited">Incomplited</option>
+                    </select><br />
 
-    </div>
-  )
+                    {editeIndex != null ? <button type='button' className="btn btn-primary" onClick={updateData}>Update</button> : <button type='submit' className='btn btn-primary'>Add comment</button>}
+
+                </form>
+            </div>
+
+            <br />
+
+
+
+       <div className='submit-tag'>
+               
+       {
+                data.map((val, index) => {
+                    return <ul>
+                        <li id='lisiOne'>{"comment:- "}{val.comment} <br /> {"date:-"} {val.date} <br /> {"status :-"}{val.status} <br />
+                            <button className="btn btn-primary" onClick={() => editComment(index)}>Edit</button>
+                            {"  "} <br /><button type='button' className="btn btn-primary" onClick={() => deleteComment(index)}>Delete</button> </li>
+                    </ul>
+                })
+            }
+       </div>
+
+        </div>
+    )
 }
